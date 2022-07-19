0becdedb5290f3494370bf0586c12d173b1037cc
Isaiah <ijandicos90@gmail.com>
2022-07-19 15:18:15 -0400

    Refactored

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+attr_reader :title
+attr_accessor :page_count, :author, :genre
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+end
